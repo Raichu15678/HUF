3122e9f97474c1a249c71791a48ce2d4a5625a9c
Raichu15678 <matefekete355@gmail.com>
2025-05-19 12:58:20 +0200

    Refactor HTML and JavaScript for currency exchange application layout and functionality

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
+//USD
+fetch("https://api.exchangerate-api.com/v4/latest/usd")
+.then(x=>x.json())
+.then(y=>megjelenitUSD(y))
 
+function megjelenitUSD(y) {
+    console.log(y)
+    document.getElementById("usd").innerHTML=y.rates.HUF+"Ft"
+}
 
+//EUR
+fetch("https://api.exchangerate-api.com/v4/latest/eur")
+.then(x=>x.json())
+.then(y=>megjelenitEUR(y))
 
+function megjelenitEUR(y) {
+    console.log(y)
+    document.getElementById("eur").innerHTML=y.rates.HUF+"Ft"
+}
 
+fetch("https://api.exchangerate-api.com/v4/latest/gbp")
+.then(x=>x.json())
+.then(y=>megjelenitGBP(y))
 
+function megjelenitGBP(y) {
+    console.log(y)
+    document.getElementById("gbp").innerHTML=y.rates.HUF+"Ft"
+}
 
+fetch("https://api.exchangerate-api.com/v4/latest/jpy")
+.then(x=>x.json())
+.then(y=>megjelenitJPY(y))
 
+function megjelenitJPY(y) {
+    console.log(y)
+    document.getElementById("jpy").innerHTML=y.rates.HUF+"Ft"
+}
 
+fetch("https://api.exchangerate-api.com/v4/latest/ron")
+.then(x=>x.json())
+.then(y=>megjelenitRON(y))
 
+function megjelenitRON(y) {
+    console.log(y)
+    document.getElementById("ron").innerHTML=y.rates.HUF+"Ft"
+}
 
+fetch("https://api.exchangerate-api.com/v4/latest/irr")
+.then(x=>x.json())
+.then(y=>megjelenitIRR(y))
 
-
-
-
-
-
+function megjelenitIRR(y) {
+    console.log(y)
+    document.getElementById("irr").innerHTML=y.rates.HUF+"Ft"
+}
 
 
 
